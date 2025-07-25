312da1ab1a77a2070ce60535dc66938dc16c58e5
Noel Dupuis <noel.dupuis45370@gmail.com>
2025-07-25 19:30:43 +0200

    Refactor PersonControllerTest to use MockitoBean for mocking PersonService

--- src/test/java/fr/noeldupuis/hdoapi/persons/controller/PersonControllerTest.java
+++ src/test/java/fr/noeldupuis/hdoapi/persons/controller/PersonControllerTest.java
@@ -9,9 +9,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.mockito.Mock;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDate;
@@ -31,7 +30,7 @@ class PersonControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private PersonService personService;
 
     @Autowired
